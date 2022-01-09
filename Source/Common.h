5e1467985209f6b99db8d5af308f45dd395ecce8
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2022-01-10 01:23:08 +0530

    added sfml graphics header for accessing render window.

--- Source/Common.h
+++ Source/Common.h
@@ -2,13 +2,16 @@
 #define COMMON_H
 
 #include "GlobalVariables.h"
+#include <SFML/Graphics.hpp>
 
 namespace Pong
 {
 	struct Common
 	{
+	public:
+		virtual void Render(sf::RenderWindow& mainRenderWindow) const = 0;
 	protected:
-		float speed = 150.0f;
+		float speed = 225.0f;
 
 		static const unsigned short outLineThickness = 3;
 	};
