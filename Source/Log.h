c9e5903c30e49b67996c530ec6ba11506152895b
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2022-01-12 19:32:25 +0530

    Log class -> class gaurds added.

--- Source/Log.h
+++ Source/Log.h
@@ -10,6 +10,14 @@ namespace Pong
 	public:
 		//End line is already added inside the function.
 		static void Print(const char* stringToPrint);
+
+		//Log should not be cloneable.
+		Log(Log& other) = delete;
+		//Log should not be assignable.
+		void operator=(const Log&) = delete;
+	private:
+		Log() = default;
+		~Log() = default;
 	};
 }
 #endif
