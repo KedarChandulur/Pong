ae5ec0049d6a7ffb764cb2966970171571c3e9ed
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2022-01-12 19:22:18 +0530

    Added comments and common header file clean up.

--- Source/CommonElementsHandler.h
+++ Source/CommonElementsHandler.h
@@ -17,26 +17,20 @@ namespace Pong
 		sf::Sound& GetSoundObject();
 		sf::Text& GetMainTextRef();
 
-		//Start Game Text Ref
 		TextHandler startgame_Text;
-		//Quit Game Text Ref
 		TextHandler quitgame_Text;
 	private:
 		//Background Sprite
 		sf::Sprite bgSprite;
-		/*sf::RectangleShape bgSprite;*/
 		//Background Texture
 		sf::Texture bgTexture;
-		//Icon and main menu image
 		sf::Image iconImage_Ref;
 
-		//Custom Font
+		//Custom Font Reference
 		sf::Font fontRef;
-		//Title Text Ref
 		TextHandler title_Text;
-		//Buffer which loads and stores audio file of ball bounce
+		//Buffer which loads and stores audio sample of ball bounce
 		sf::SoundBuffer ballSB;
-		//Sfml sound object for playing the ball bounce sound
 		sf::Sound ballBounceSound;		
 	};
 }
