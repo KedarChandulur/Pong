a745dca4570f255c26e4b490ca501eca155d4b52
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2022-01-12 15:48:16 +0530

    made const infront of unsigned.

--- Source/GlobalVaraibles.cpp
+++ Source/GlobalVaraibles.cpp
@@ -2,8 +2,8 @@
 
 namespace Pong
 {
-	unsigned const short SCREEN_WIDTH = 1280;
-	unsigned const short SCREEN_HEIGHT = SCREEN_WIDTH / 16 * 9;
+	const unsigned short SCREEN_WIDTH = 1280;
+	const unsigned short SCREEN_HEIGHT = SCREEN_WIDTH / 16 * 9;
 
 	bool inGame = false;
 }
