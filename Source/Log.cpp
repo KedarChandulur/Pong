8545a8f665f6c035a9cb9187867a2f49b2fde308
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2022-01-26 17:56:21 +0530

    Log function templated.

--- Source/Log.cpp
+++ Source/Log.cpp
@@ -1,10 +1,30 @@
 #include "Log.h"
 
-void Pong::Log::Print(const char* stringToPrint)
-{
-#if _DEBUG
-	std::cout << stringToPrint << std::endl;
-#elif NDEBUG
-	//Log to file.
-#endif
-}
+//void Pong::Log::Print(const char* stringToPrint)
+//{
+//#if _DEBUG
+//	std::cout << stringToPrint << std::endl;
+//#elif NDEBUG
+//	//Log to file.
+//#endif
+//}
+
+//template<typename T>
+//void Pong::Log::Print(T& log)
+//{
+//#if _DEBUG
+//	std::cout << log << std::endl;
+//#elif NDEBUG
+//	//Log to file.
+//#endif
+//}
+//
+//template<typename T>
+//void Pong::Log::Print(T* log)
+//{
+//#if _DEBUG
+//	std::cout << log << std::endl;
+//#elif NDEBUG
+//	//Log to file.
+//#endif
+//}
